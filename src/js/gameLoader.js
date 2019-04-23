1c9b6c45c5e4aa149e47b5aee6c53ae077f741d3
Daniel M <daniel.murth@chello.at>
2019-04-23 13:49:01 +0200

    [REFACTORING] to gulp

--- src/js/gameLoader.js
+++ src/js/gameLoader.js
@@ -1,13 +1,13 @@
 
 
-var ajaxhttp = new XMLHttpRequest();   
-var url = "https://api.myjson.com/bins/bsui0"; //it doesn't enable to use (assets/json/games.json)//{}myjson http://myjson.com/ihhge 
+var ajaxhttp = new XMLHttpRequest();
+var url = "https://api.myjson.com/bins/bsui0"; //it doesn't enable to use (assets/json/games.json)//{}myjson http://myjson.com/ihhge
 var x = "";
 var myObj = "";
 
 
 function displayAllImages() {
-  ajaxhttp.open("GET", url, true);  //get will be used to request dats//post will be used when the dats are more then 512Bytes //if the boolean is true it won'z be wainting for the request, but it is false it will(it is to be used for a Login field) 
+  ajaxhttp.open("GET", url, true);  //get will be used to request dats//post will be used when the dats are more then 512Bytes //if the boolean is true it won'z be wainting for the request, but it is false it will(it is to be used for a Login field)
   ajaxhttp.setRequestHeader("content-type", "application/json"); //Assigns the header a label or value pair, which is sent with the request
   ajaxhttp.onreadystatechange = function () {   //will be called by each change of the XHR-State
     if (ajaxhttp.readyState == 4 && ajaxhttp.status == 200) { //the number gives the state about it, if it ranges 4 it is done//ready state gives a state about the progress //sends the number of output code for example 200 for OK
@@ -19,33 +19,33 @@ function displayAllImages() {
       generateSubMenu();
       filterElements();
       activeTarget();
-    }  
+    }
   }
   ajaxhttp.send(null); //sends the request, optional with a string or Dom-Object-Dats
-} 
+}
 
 function generateGames() {
-  for (i in myObj.searchcriteria) { 
+  for (var i in myObj.searchcriteria) {
     x += "<div class='content__container-games-card " + myObj.searchcriteria[i].tags.join(' ') + ' ' + myObj.searchcriteria[i].console +
-    "'><div class='content__container-games-card-front'><img class='content__container-games-card-images ' src='" + myObj.searchcriteria[i].src + "'/></div><div class='content__container-games-card-back'><div class='content__container-games-card-back-content'><h2 class='content__container-games-card-back-headline'>" + 
-    myObj.searchcriteria[i].name + "</h2><p class='content__container-games-card-back-text'>" + myObj.searchcriteria[i].tags.sort().join(', ') + ", " + myObj.searchcriteria[i].console + "</p><a class='content__container-games-card-back-links' href='" + 
+    "'><div class='content__container-games-card-front'><img class='content__container-games-card-images ' src='" + myObj.searchcriteria[i].src + "'/></div><div class='content__container-games-card-back'><div class='content__container-games-card-back-content'><h2 class='content__container-games-card-back-headline'>" +
+    myObj.searchcriteria[i].name + "</h2><p class='content__container-games-card-back-text'>" + myObj.searchcriteria[i].tags.sort().join(', ') + ", " + myObj.searchcriteria[i].console + "</p><a class='content__container-games-card-back-links' href='" +
     myObj.searchcriteria[i].url + "' target='_blank'><i class='fab fa-youtube'></i><a/></div></div></div>";
   }
   document.getElementById('content__container-games').innerHTML += x;
 }
 
-/* generate the main Menu (but it isn't able to use (i need to loop only one time not 2 times, then it will func))*/ 
+/* generate the main Menu (but it isn't able to use (i need to loop only one time not 2 times, then it will func))*/
 function generateMenu() {
   var consoleArray = [];
   var consoleList = "";
 
-  for (i in myObj.searchcriteria) {
+  for (var i in myObj.searchcriteria) {
     if (consoleArray.indexOf(myObj.searchcriteria[i].console) == -1) {
       consoleArray.push(myObj.searchcriteria[i].console);
     }
   }
 
-  for (i in consoleArray) {
+  for (var i in consoleArray) {
     consoleList += "<li class='content__header-menu-toggle' data-filter='" + consoleArray[i] + "'>" + consoleArray[i] + "<span class='content__header-menu-border'></span><span class='content__header-menu-border'></span><span class='content__header-menu-border'></span><span class='content__header-menu-border'></span></li>";
   }
   document.getElementById('content__header-menu').innerHTML = "<li class='content__header-menu-toggle' data-filter='all'> Show all<span class='content__header-menu-border'></span><span class='content__header-menu-border'></span><span class='content__header-menu-border'></span><span class='content__header-menu-border'></span></li>" + consoleList;
@@ -56,16 +56,16 @@ function generateSubMenu() {
   var tagArray = [];
   var tagList = "";
 
-  for (i in myObj.searchcriteria) {
-    for (j in myObj.searchcriteria[i].tags) {
+  for (var i in myObj.searchcriteria) {
+    for (var j in myObj.searchcriteria[i].tags) {
       if (tagArray.indexOf(myObj.searchcriteria[i].tags[j]) == -1) {
         tagArray.push(myObj.searchcriteria[i].tags[j]);
       }
     }
-  } 
+  }
   tagArray.sort();
 
-  for (i in tagArray) {
+  for (var i in tagArray) {
     tagList += "<li class='content__header-sub-menu-toggle content__header-menu-toggle' data-filter='" + tagArray[i] + "'>" + tagArray[i] + "</li>";
   }
   document.getElementById('content__header-sub-menu').innerHTML = tagList;
@@ -73,11 +73,11 @@ function generateSubMenu() {
 
 
 function filterElements() {
-  var menuBtn = document.querySelectorAll('.content__header-menu-toggle'); 
+  var menuBtn = document.querySelectorAll('.content__header-menu-toggle');
   console.log(menuBtn);
   menuBtn.forEach(function(item) {               //[...z] change a nodelist into an array (need to look)
     item.addEventListener('click', function(e){ //e = event
-      var filter = e.target.dataset.filter; // the event from the element which is choosen with the data-filter 
+      var filter = e.target.dataset.filter; // the event from the element which is choosen with the data-filter
 
       var gameCards = document.getElementsByClassName("content__container-games-card");
       if (filter == "all") {
@@ -94,11 +94,11 @@ function filterElements() {
     });
   });
 }
-      
+
 function activeTarget() {
-  var menu = document.querySelectorAll('.content__header-menu-toggle'); 
+  var menu = document.querySelectorAll('.content__header-menu-toggle');
   menu.forEach(function(menuItem) {               //[...z] change a nodelist into an array (need to look)
-    menuItem.addEventListener('click', function(event){ 
+    menuItem.addEventListener('click', function(event){
       menu.forEach(function(menuItem) {   //loop throw the menus and remove this class
         menuItem.classList.remove('content__header-menu--active');
       });
@@ -107,7 +107,7 @@ function activeTarget() {
   });
 }
 
-     
+
 
 //todos: make a sort btn
 //make a nightmode Btn
@@ -117,5 +117,3 @@ function activeTarget() {
 //change the css in scss
 //look git-hub
 //refactor js functions
-
-
